d7a69ab04a65372c06b01ef94ebcc523bdbe1d78
MrT3313 <rturgeon13@gmail.com>
2019-03-13 20:17:47 -0500

    images rendering w/new data

--- instagram/instagram/src/components/postContainer.js
+++ instagram/instagram/src/components/postContainer.js
@@ -18,14 +18,16 @@ function PostContainer(props) {
                     </div>
                 </div>
                 <div className='postIMG'>
-                    <img src="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjzptfpiYDhAhVRS6wKHVPGCdUQjRx6BAgBEAU&url=https%3A%2F%2Fwww.123rf.com%2Fphoto_82766200_stock-vector-winking-smiley-gesturing-with-his-hand-emoticon-thumbs-up-showing-positive-mood-.html&psig=AOvVaw15-JFPtjFK_3SsadC8Ufy8&ust=1552599451913638" alt="placeholderImg" />
+                    {/* <img src="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjzptfpiYDhAhVRS6wKHVPGCdUQjRx6BAgBEAU&url=https%3A%2F%2Fwww.123rf.com%2Fphoto_82766200_stock-vector-winking-smiley-gesturing-with-his-hand-emoticon-thumbs-up-showing-positive-mood-.html&psig=AOvVaw15-JFPtjFK_3SsadC8Ufy8&ust=1552599451913638" alt="placeholderImg" /> */}
+
+                    <img src={post.imageUrl} />
                 </div>
                 <div className='container-postBottom'>
                     <div className='postInteraction'>
                         <i class="far fa-heart"></i>
                         <i class="far fa-comment"></i>
                         <div className='likeCounter'>
-                            <div>### likes</div>
+                            <div>{post.likes} likes</div>
                         </div>
                     </div>
                     <div className='container-comments'>
