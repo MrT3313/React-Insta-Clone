5052db3be3dcde4db0409ebd056bf81fb987ad34
MrT3313 <rturgeon13@gmail.com>
2019-03-13 20:42:49 -0500

    attempt @ typechecking on PostContainer not working

--- instagram/instagram/src/components/postContainer.js
+++ instagram/instagram/src/components/postContainer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 import Comment from './Comment'
 
@@ -11,7 +12,7 @@ function PostContainer(props) {
             <div className='container-post'>
                 <div className='container-postHeader'>
                     <div className='profile-IMG'>
-                        <img src={post.thumbnailUrl} />
+                        <img src={post.thumbnailUrl} alt='img'/>
                     </div>
                     <div className='userName'>
                         {post.username}
@@ -20,12 +21,12 @@ function PostContainer(props) {
                 <div className='postIMG'>
                     {/* <img src="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjzptfpiYDhAhVRS6wKHVPGCdUQjRx6BAgBEAU&url=https%3A%2F%2Fwww.123rf.com%2Fphoto_82766200_stock-vector-winking-smiley-gesturing-with-his-hand-emoticon-thumbs-up-showing-positive-mood-.html&psig=AOvVaw15-JFPtjFK_3SsadC8Ufy8&ust=1552599451913638" alt="placeholderImg" /> */}
 
-                    <img src={post.imageUrl} />
+                    <img src={post.imageUrl} alt='img'/>
                 </div>
                 <div className='container-postBottom'>
                     <div className='postInteraction'>
-                        <i class="far fa-heart"></i>
-                        <i class="far fa-comment"></i>
+                        <i className="far fa-heart"></i>
+                        <i className="far fa-comment"></i>
                         <div className='likeCounter'>
                             <div>{post.likes} likes</div>
                         </div>
@@ -37,12 +38,20 @@ function PostContainer(props) {
                     </div>
                     <div className='addComment'>
                         <input type="text" name='addComment' placeholder='Add a comment...'/>
-                        <i class="fas fa-ellipsis-h"></i>
+                        <i className="fas fa-ellipsis-h"></i>
                     </div>
                 </div>
 
             </div>
         )
+}
+
+PostContainer.propTypes = {
+    thumbnailUrl: PropTypes.number,
+    imageUrl: PropTypes.string,
+    username: PropTypes.string,
+    likes: PropTypes.number,
+    comments: PropTypes.array
 }
 
 export default PostContainer 
--- instagram/instagram/src/components/searchBar.js
+++ instagram/instagram/src/components/searchBar.js
@@ -3,13 +3,12 @@ import React from 'react'
 import './components.css'
 
 function SearchBar(props) {
-    const { post }  = props;
 
         return (
             <div className='container-searchBar'>
                 <div className='searchBar-left'>
                     <div className='instagramLogo'>
-                        <i class="fab fa-instagram"></i>
+                        <i className="fab fa-instagram"></i>
                     </div>
                     <div className='instagramName'>
                         <h2>Instagram</h2>
@@ -19,9 +18,9 @@ function SearchBar(props) {
                     <input type="text" name='searchBar' placeholder='Search'/>
                 </form>
                 <div className='searchBar-right'>
-                    <i class="far fa-compass"></i>
-                    <i class="far fa-heart"></i>
-                    <i class="far fa-user"></i>
+                    <i className="far fa-compass"></i>
+                    <i className="far fa-heart"></i>
+                    <i className="far fa-user"></i>
                 </div>
             </div>
         )
