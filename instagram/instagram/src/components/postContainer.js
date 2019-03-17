5522e9ae04585bf4a5565adeabed627f8a0b4565
MrT3313 <rturgeon13@gmail.com>
2019-03-17 05:42:21 -0500

    bumped PostHeader into own functional component

--- instagram/instagram/src/components/postContainer.js
+++ instagram/instagram/src/components/postContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import CommentContainer from './CommentContainer'
+import PostHeader from './PostHeader'
 
 import './components.css'
 
@@ -9,29 +10,46 @@ class PostContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            liked: false
         }
         console.log('post container CONSTRUCTOR invoked')
+    }
+
+    onClickHelper = e => {
+        // GET BUTTON ELEMENT
+        const heartBtn = document.getElementsByClassName('fa-heart')
+        console.log(heartBtn)
+        
+        if (this.state.liked !== false) {
+
+        }
+    }
+
+    onDoubleClickHelper = e => {
+
     }
 
     render() {
         console.log('post container RENDER invoked')
         return (
             <div className='container-post'>
-                <div className='container-postHeader'>
-                    <div className='profile-IMG'>
-                        {/* <img src={post.thumbnailUrl} alt='img'/> */}
-                    </div>
-                    <div className='userName'>
-                        {this.props.post.username}
-                    </div>
-                </div>
+                <PostHeader 
+                    thumbnailUrl={this.props.post.thumbnailUrl}
+                    username={this.props.post.username}
+                />
+
+
+
                 <div className='postIMG'>
                     <img src={this.props.post.imageUrl} alt='img'/>
                 </div>
                 <div className='container-postBottom'>
                     <div className='postInteraction'>
-                        <i className="far fa-heart"></i>
+                        <i 
+                            className="far fa-heart false" 
+                            onClick={this.onClickHelper}
+                            // onDoubleClick={this.onDoubleClickHelper}
+                        ></i>
                         <i className="far fa-comment"></i>
                         <div className='likeCounter'>
                             <div>{this.props.post.likes} likes</div>
