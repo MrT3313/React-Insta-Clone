8995028432f8956ba84637d8d2c43ac9c20dda4b
MrT3313 <rturgeon13@gmail.com>
2019-03-21 22:11:07 -0500

    SearchBar css changed to styled components

--- instagram/instagram/src/components/SearchBar/SearchBar.js
+++ instagram/instagram/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,50 @@
-import React from 'react'
+// -IMPORTS- // 
+    import React from 'react'
 
-import '../components.css'
+    // CSS
+    import styled from 'styled-components'
+
+const Section = styled.section`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    div {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .instagramName {
+        padding-left: 15px;
+    }
+    .instagramLogo {
+        padding: 0px 15px;
+        border-right: 1px solid black;
+    }
+    .fab {
+        font-size: 30px;
+    }
+    .far {
+        font-size: 20px;
+        padding-right: 15px;
+    }
+`;
+
+const Form = styled.form`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    input {
+        background: rgb(230, 229, 229);
+        height: 20px;
+        text-align: center;
+    }
+`;
 
 function SearchBar(props) {
-
         return (
-            <div className='container-searchBar'>
+            <Section className='container-searchBar'>
                 <div className='searchBar-left'>
                     <div className='instagramLogo'>
                         <i className="fab fa-instagram"></i>
@@ -14,7 +53,7 @@ function SearchBar(props) {
                         <h2>Instagram</h2>
                     </div>
                 </div>
-                <form className='searchBar-center'>
+                <Form className='searchBar-center'>
                     <input 
                         type="text" 
                         name='searchBar' 
@@ -22,13 +61,13 @@ function SearchBar(props) {
 
                         onChange={props.filterSearchText}
                     />
-                </form>
+                </Form>
                 <div className='searchBar-right'>
                     <i className="far fa-compass"></i>
                     <i className="far fa-heart"></i>
                     <i className="far fa-user"></i>
                 </div>
-            </div>
+            </Section>
         )
 }
 
